4a266d1d4edf700d8a00fb9b61c3368273e4b5dd
Lucas Blandon <lucasblandon@gmail.com>
2022-02-17 18:36:14 -0500

    Cambio en la apk

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_peliculas/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -13,7 +14,7 @@ class HomeScreen extends StatelessWidget {
         ),
         body: Column(
           children: [
-            //TODO: CardSwiper
+            CardSwiper()
 
             //lsitado horizanota
           ],
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,1 @@
+export 'package:flutter_peliculas/widgets/card_swiper.dart';
